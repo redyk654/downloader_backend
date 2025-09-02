f5954e5b6200ba83af99b9a0846d9b409a4d323e
redyk654 <chrisppoyoumbissi39@gmail.com>
2025-09-02 10:34:35 +0100

    [fix] settings.py mis a jour pour prometheus

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    'django_prometheus',
     'books.apps.BooksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_prometheus',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
